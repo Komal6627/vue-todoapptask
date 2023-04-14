35a585ec1e275ba42572d72aca9f71084f446096
Komal6627 <85746813+Komal6627@users.noreply.github.com>
2023-04-14 09:41:21 +0530

    import icons in main file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-const { createApp } = require('vue');
-import App from "./App.vue";
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-createApp(App).mount("#app");
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const { createApp } = require('vue');
+import App from './App.vue';
+
+createApp(App).mount('#app');
